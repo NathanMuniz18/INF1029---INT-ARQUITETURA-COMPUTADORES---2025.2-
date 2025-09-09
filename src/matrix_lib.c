9b3d77e19966b7645a25a49294a4ac8bca23c856
vagnermcj <vagnermessias.vmc@gmail.com>
2025-09-09 18:07:46 -0300

    Ajuste matrixlib

--- src/matrix_lib.c
+++ src/matrix_lib.c
@@ -19,34 +19,6 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix) {
     return 1; // sucesso
 }
 
-/*
-int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC) {
-    if (matrixA == NULL || matrixB == NULL || matrixC == NULL) {
-        return 0; // erro
-    }
-
-    if (matrixA->width != matrixB->height) {
-        return 0; // dimensões incompatíveis
-    }
-
-    if (matrixC->height != matrixA->height || matrixC->width != matrixB->width) {
-        return 0; // matriz C com dimensões erradas
-    }
-
-    for (unsigned long int i = 0; i < matrixA->height; i++) {   // percorre as linhas de A
-        for (unsigned long int j = 0; j < matrixB->width; j++) { // percorre as colunas de B
-            float sum = 0.0;
-            for (unsigned long int k = 0; k < matrixA->width; k++) { // percorre cada elemento da linha i de A e da coluna j de B
-                float a = matrixA->rows[i * matrixA->width + k];
-                float b = matrixB->rows[k * matrixB->width + j];
-                sum += a * b;
-            }
-            matrixC->rows[i * matrixC->width + j] = sum; // resultado em C[i][j]
-        }
-    }
-    return 1; // sucesso
-}*/
-
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC) {
     if (matrixA == NULL || matrixB == NULL || matrixC == NULL) {
         return 0; // erro
@@ -75,3 +47,32 @@ int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct ma
     }
     return 1;
 }
+
+/* Versão inicial
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC) {
+    if (matrixA == NULL || matrixB == NULL || matrixC == NULL) {
+        return 0; // erro
+    }
+
+    if (matrixA->width != matrixB->height) {
+        return 0; // dimensões incompatíveis
+    }
+
+    if (matrixC->height != matrixA->height || matrixC->width != matrixB->width) {
+        return 0; // matriz C com dimensões erradas
+    }
+
+    for (unsigned long int i = 0; i < matrixA->height; i++) {   // percorre as linhas de A
+        for (unsigned long int j = 0; j < matrixB->width; j++) { // percorre as colunas de B
+            float sum = 0.0;
+            for (unsigned long int k = 0; k < matrixA->width; k++) { // percorre cada elemento da linha i de A e da coluna j de B
+                float a = matrixA->rows[i * matrixA->width + k];
+                float b = matrixB->rows[k * matrixB->width + j];
+                sum += a * b;
+            }
+            matrixC->rows[i * matrixC->width + j] = sum; // resultado em C[i][j]
+        }
+    }
+    return 1; // sucesso
+}*/
+
